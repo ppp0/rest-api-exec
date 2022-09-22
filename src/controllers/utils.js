1e4b737888485b539a06c778f437c51c686bab8e
ppp0 <philipp.pfaff@cargomedia.ch>
2022-09-22 16:22:10 +0200

    Refine endpoints

--- src/controllers/utils.js
+++ src/controllers/utils.js
@@ -1,17 +1,21 @@
 'use strict';
-var UglifyJS = require("uglify-js");
-var Browserify = require('browserify')
-var fs = require('fs');
+const UglifyJS = require("uglify-js");
+const Browserify = require('browserify')
+const fs = require('fs');
+const path = require('path');
+
 
 module.exports = {
   uglify: (ctx) => {
-    ctx.body = UglifyJS.minify(ctx.request.body, {quote_keys: true}).code;
+    ctx.body = UglifyJS.minify(ctx.request.body, {output: {quote_keys: true, beautify: false}}).code;
   },
   browserify: (ctx) => {
     var options = ctx.request.body;
     ctx.assert(options.mainPaths, 422, 'Property "mainPaths" must be defined');
     ctx.assert(Array.isArray(options.mainPaths), 422, 'Property "mainPaths" must be an array');
-    options.mainPaths.forEach((val) => ctx.assert(fs.stat(val, (err, stats) => stats.isFile()), 422, 'File ' + val + ' not found'));
+    // options.mainPaths.forEach(val => {
+    //   ctx.assert(path.existSync(val), 422, val + ' not found');
+    // });
     var browserify = new Browserify(options.mainPaths, {paths: options.sourcePaths});
     ctx.body = browserify.bundle();
   }
